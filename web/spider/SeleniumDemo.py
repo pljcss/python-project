d05ed7a8f9f87f43d5f423c5117b06afd3f5bdcf
caosai <caosai@nonobank.com>
2019-06-25 17:50:42 +0800

    截图

--- web/spider/SeleniumDemo.py
+++ web/spider/SeleniumDemo.py
@@ -2,11 +2,17 @@ from selenium import webdriver
 
 
 if __name__ == '__main__':
+    # 调用 chrome 驱动
     driver = webdriver.Chrome()
 
-    driver.get("https://www.zhihu.com/signup?next=%2F")
+    driver.set_window_size(1200, 900)
+
+    # driver.get("https://www.zhihu.com/signup?next=%2F")
+    driver.get("https://cuiqingcai.com/1076.html")
+
     print(driver.page_source)
     # driver.close()
 
-
-
+    # 截图
+    driver.save_screenshot("codingpy.png")
+    driver.close()
