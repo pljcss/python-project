0871a981a5f13d05e3a1b435c3f6e9a6ce51bf50
caosai <caosai@nonobank.com>
2019-08-14 11:24:45 +0800

    动态代理

--- test/test2.py
+++ test/test2.py
@@ -1,14 +1,64 @@
+import random
 
+
+def m1(*number):
+    # total = 0
+    # for i in number:
+    #     total = total + i
+    #
+    # print(total)
+
+    m2(*number)
+
+    print("-"*20)
+
+def m2(*number):
+    total = 0
+    for i in number:
+        total = total + i
+
+    print(total)
+
+
+
+def m3():
+    a = [1, 2, 0]
+    num = random.choice(a)
+    print("被除术 ",num)
+    return num
+
+def m4():
+    a = [1, 2, 0]
+
+    retry_numbers=0
+
+    while retry_numbers<10:
+        num = random.choice(a)
+        print("选取的随机数 ", num)
+
+        # 随机数为0, 则停止循环
+        if num==0:
+            return num
+
+        retry_numbers +=1
 
 
 if __name__ == '__main__':
 
-    list_1 = []
-    for i in range(100):
-        dict_1 = dict() # 此种方式定义dict, 需要带括号
-        dict_1["name"] = "Bob"+ str(i)
-        dict_1["score"] = i
+    m4()
 
-        list_1.append(dict_1)
+    # m1(1, 2, 3, 4)
 
-    print(list_1)
+    # try:
+    #     for i in range(10):
+    #         print(i)
+    #         print(i/m3())
+    # except Exception as e:
+    #     print(e)
+
+    #
+    # for i in range(10):
+    #     try:
+    #         print(i/m3())
+    #     except Exception as e:
+    #         print(e)
